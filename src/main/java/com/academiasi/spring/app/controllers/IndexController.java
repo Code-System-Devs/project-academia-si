9235fa9a8a88d610d3ce4cd7cdc3cf4f88f004ff
Samayoa <Brayton-samayoa@hotmail.com>
2020-04-12 10:42:23 -0600

    Html nosotros falta los estilos

---
+++ src/main/java/com/academiasi/spring/app/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package com.academiasi.spring.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+	
+	@GetMapping({"/index","/","/home"})
+	public String Index() {
+		return"index";
+	}
+
+}
