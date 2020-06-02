f8ed73dd05f3b9de40ca79312d5da4c2bc92b5d5
“Michael1481” <umgacademia.soft@gmail.com>
2020-06-02 16:52:50 -0600

    Creando el modulo Alumnos - creando paquete entity

---
+++ src/main/java/com/academiasi/spring/app/models/entities/Alumno.java
@@ -0,0 +1,10 @@
+package com.academiasi.spring.app.models.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="alumnos")
+public class Alumno {
+
+}
