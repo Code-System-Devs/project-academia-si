c3f04873d98f518ed09be7501bbf185dbd6ad937
“Michael1481” <umgacademia.soft@gmail.com>
2020-06-08 12:33:26 -0600

    Deploy del Prototipo 1 a Heroku con BD PostgreSQL

--- src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO horarios (id, horainicio, horafin, curso, instructor) VALUES(1, '8:00', '10:00', 'Operador', 'Carlos');
-INSERT INTO horarios (id, horainicio, horafin, curso, instructor) VALUES(2, '10:30', '12:30', 'Tecnico I', 'Marvin');
