37ca81b604257ed65acfd1b9d43ec3ed2f1dec1b
“Michael1481” <umgacademia.soft@gmail.com>
2020-06-07 18:37:20 -0600

    Creando permisos para los usuarios

--- src/main/java/com/academiasi/spring/app/SpringSecurityConfig.java
+++ src/main/java/com/academiasi/spring/app/SpringSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.User.UserBuilder;
@@ -17,6 +18,18 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
+
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http.authorizeRequests().antMatchers("/","/css/**", "/js/**","/images/**", "/horarios","/eventos").permitAll()
+		.antMatchers("/form-horarios/**").hasAnyRole("ADMIN")
+		.antMatchers("/eliminar/**").hasAnyRole("ADMIN")
+		.antMatchers("/nosotros/").hasAnyRole("ADMIN")
+		.anyRequest().authenticated();
+	}
+
 
 	@Autowired
 	public void configurerGlobal(AuthenticationManagerBuilder builder) throws Exception
