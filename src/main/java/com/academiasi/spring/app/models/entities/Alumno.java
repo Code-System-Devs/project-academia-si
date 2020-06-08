1f2acf45e02ec771cb83a2fed2a31c269a89804b
“Michael1481” <umgacademia.soft@gmail.com>
2020-06-08 16:35:02 -0600

    Arreglando problemas del identity del ID

--- src/main/java/com/academiasi/spring/app/models/entities/Alumno.java
+++ src/main/java/com/academiasi/spring/app/models/entities/Alumno.java
@@ -20,7 +20,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 public class Alumno implements Serializable {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@NotEmpty
--- src/main/java/com/academiasi/spring/app/models/entities/Horario.java
+++ src/main/java/com/academiasi/spring/app/models/entities/Horario.java
@@ -14,7 +14,7 @@ import javax.validation.constraints.NotEmpty;
 public class Horario implements Serializable {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@NotEmpty
