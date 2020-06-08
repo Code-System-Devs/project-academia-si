c6a25cf07954db6559fd4455829085def2af2719
“Michael1481” <umgacademia.soft@gmail.com>
2020-06-07 19:05:03 -0600

    Creando formulario de login y error 403

--- src/main/java/com/academiasi/spring/app/SpringSecurityConfig.java
+++ src/main/java/com/academiasi/spring/app/SpringSecurityConfig.java
@@ -27,7 +27,11 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
 		.antMatchers("/form-horarios/**").hasAnyRole("ADMIN")
 		.antMatchers("/eliminar/**").hasAnyRole("ADMIN")
 		.antMatchers("/nosotros/").hasAnyRole("ADMIN")
-		.anyRequest().authenticated();
+		.anyRequest().authenticated()
+		.and()
+		.formLogin().permitAll()
+		.and()
+		.logout().permitAll();
 	}
 
 
