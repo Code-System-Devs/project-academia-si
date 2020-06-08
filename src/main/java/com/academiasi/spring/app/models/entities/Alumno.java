e10e564e6ce1dc5ea86b89f3720114219483f0e7
“Michael1481” <umgacademia.soft@gmail.com>
2020-06-08 12:42:56 -0600

    Arreglando errores Post sql

--- src/main/java/com/academiasi/spring/app/models/entities/Alumno.java
+++ src/main/java/com/academiasi/spring/app/models/entities/Alumno.java
@@ -20,7 +20,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 public class Alumno implements Serializable {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	private Long id;
 
 	@NotEmpty
--- src/main/java/com/academiasi/spring/app/models/entities/Horario.java
+++ src/main/java/com/academiasi/spring/app/models/entities/Horario.java
@@ -14,7 +14,7 @@ import javax.validation.constraints.NotEmpty;
 public class Horario implements Serializable{
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	private Long id;
 	
 	@NotEmpty
